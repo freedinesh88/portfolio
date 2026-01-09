ed746edba710aab59647f94b68d03795992c501d
freedinesh88 <freedinesh88@gmail.com>
2026-01-09 18:15:40 +0530

    DAY:3 Overlay menu done

--- MY_MODERN_PORTFOLIO/src/components/OverlayMenu.jsx
+++ MY_MODERN_PORTFOLIO/src/components/OverlayMenu.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
-
-const OverlayMenu = () => {
+import {AnimatePresence} from 'framer-motion';
+import { FiX } from "react-icons/fi";
+export default function OverlayMenu({ isOpen, onClose }) {
+  const isMobile= typeof window !== 'undefined' && window.innerWidth <= 1024;
+  const origin = isMobile ? "95% 8%" : "50% 8%";
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div className="fixed inset-0 bg-opacity-95 flex flex-col items-center justify-center z-50"
+        initial={{clipPath: `circle(0% at ${origin})`}}
+        animate={{clipPath: `circle(150% at ${origin})`}}
+        exit={{clipPath: `circle(0% at ${origin})`}}
+        transition={{duration: 0.7, ease: [0.4,0,0.2,1] }}
+        style={{backgroundColor:"rgba(0,0,0,0.95)"}}
+        >
+          <button onClick={onClose}
+          className='absolute top-6 right-6 text-white text-3xl'
+          aria-label='Close menu'>
+            <FiX />
+          </button>
 
-export default OverlayMenu
+          <ul className='space-y-6 text-center'>
+            {['Home', 'About', 'Skills', 'Projects', 'Experience', 'Testimonials','Contact'].map((item, index) => (
+              <motion.li 
+              key={item}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ delay: 0.3+index * 0.1 }}
+              className="text-white text-2xl font-medium hover:text-gray-300"
+            >
+              <a href={`#${item.toLowerCase()}`} onClick={onClose} 
+              className='text-4xl text-white font-semibold hover:text-pink-400 transition-colors duration-300'>
+                {item}
+              </a>
+            </motion.li>
+          ))}
+
+          </ul>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
